56c7459dc444f1845ee79513679d8c6f54047ef5
Bruna Santiago <brunasanthiago@outlook.com>
2023-01-09 17:16:22 -0300

    finalizado

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -13,7 +13,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(400).json({ error: 'Price not found' });
   };
 
-  const success_Url = `${process.env.NEXT_URL}/sucess?session_id={CHECKOUT_SESSION_ID}`;
+  const success_Url = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`;
   const cancel_Url = `${process.env.NEXT_URL}/`;
 
   const checkoutSession = await stripe.checkout.sessions.create({

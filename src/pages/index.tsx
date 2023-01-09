b7d5bee4bc022f9e513fa22d86bf0c9aadd23258
Bruna Santiago <brunasanthiago@outlook.com>
2023-01-09 09:44:17 -0300

    começando a pagina de produtos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,13 @@
 import { HomeContainer, Product } from "../styles/pages/home";
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { useKeenSlider } from 'keen-slider/react';
-
-import camiseta1 from '../assets/camisetas/1.png';
-import camiseta2 from '../assets/camisetas/2.png';
-import camiseta3 from '../assets/camisetas/3.png';
 
 import 'keen-slider/keen-slider.min.css';
 
 import { stripe } from '../lib/stripe';
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Stripe from "stripe";
 
 
@@ -36,20 +33,22 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className='keen-slider'>
       {products.map(product => {
         return (
-        <Product key={product.id}className='keen-slider__slide'>
-          <Image src={product.image} width={520} height={400} alt="" />
-          <footer>
-          <strong>{product.title}</strong>
-          <span>R$ {product.price}</span>
-          </footer>
-        </Product>
+          <Link key={product.id} href={`/product/${product.id}`}>
+            <Product className='keen-slider__slide'>
+              <Image src={product.image} width={520} height={400} alt="" />
+              <footer>
+              <strong>{product.title}</strong>
+              <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
   )
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   });
@@ -59,7 +58,10 @@ export const getServerSideProps: GetServerSideProps = async () => {
     return {
       id: product.id,
       title: product.name,
-      price: price.unit_amount! / 100,
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+      }).format(price.unit_amount! / 100),
       image: product.images[0],
   }
 });
@@ -67,6 +69,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       products,
-    }
+    },
+    revalidate: 60 * 60 * 2,
   }
 }
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,10 +1,26 @@
-import { useRouter } from "next/router";
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product";
+import Image from 'next/image';
+import camiseta1 from '../../assets/camisetas/1.png';
+import { GetStaticProps } from "next";
 
 export default function Product() {
-    const { query } = useRouter();
   return (
-    <div>
-      <h1>Product: {JSON.stringify(query)}</h1>
-    </div>
+    <ProductContainer>
+      <ImageContainer>
+        <Image src={camiseta1} alt={""} />
+      </ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta 1</h1>
+        <span>R$ 79,90</span>
+        <button>Comprar</button>
+      </ProductDetails>
+    </ProductContainer>
   )
 };
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {},
+    revalidate: 60 * 60 * 1, //1 hour
+  }
+}
